3048c6bcb4c84b54532ffc518c87a5d56ebb91d4
Lucille Gris <lucille.gris@gmail.com>
2024-11-13 17:07:14 +0100

    exercice 3 non terminé (sauvegarde) - rails simple airbnb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,12 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+7.times do |index|
+  f_name = "name_#{index}"
+  f_addr = "address_details_#{index}"
+  f_desc = "description_#{index}: aaaaaaa a aaaaaaa aa aaaaaaaaaaaaaaaa aaaaaaaaa aaaa"
+  f_price = (0..100).to_a.sample
+  f_guests = (0..10).to_a.sample
+  Flat.create!(name: f_name, address: f_addr, description: f_desc, price_per_night: f_price, number_of_guests: f_guests)
+end
